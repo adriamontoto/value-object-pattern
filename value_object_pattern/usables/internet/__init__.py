d80470da00b76e340f0cc09939bed30fbe1996be
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-01-04 22:50:41 +0100

    ✨ feat: implement Ipv6AddressValueObject for managing IPv6 addresses

--- value_object_pattern/usables/internet/__init__.py
+++ value_object_pattern/usables/internet/__init__.py
@@ -1,9 +1,11 @@
 from .ipv4_address_value_object import Ipv4AddressValueObject
+from .ipv6_address_value_object import Ipv6AddressValueObject
 from .mac_address_value_object import MacAddressValueObject
 from .port_value_object import PortValueObject
 
 __all__ = (
     'Ipv4AddressValueObject',
+    'Ipv6AddressValueObject',
     'MacAddressValueObject',
     'PortValueObject',
 )
