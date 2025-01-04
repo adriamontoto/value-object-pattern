976b0226acfbb95632c262e430cecfd724caca64
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-01-04 10:56:03 +0100

    feat: implement validation method in value object class

--- value_object_pattern/models/value_object.py
+++ value_object_pattern/models/value_object.py
@@ -31,6 +31,8 @@ class ValueObject(ABC, Generic[T]):
         Args:
             value (T): Value.
         """
+        self._validate(value=value)
+
         object.__setattr__(self, '_value', value)
 
     @override
@@ -102,6 +104,27 @@ class ValueObject(ABC, Generic[T]):
             raise AttributeError(f'Cannot modify attribute "{public_key}" of immutable instance.')
 
         raise AttributeError(f'{self.__class__.__name__} object has no attribute "{key}".')
+
+    def _process(self, value: T) -> T:
+        """
+        This method processes the value object value before storage.
+
+        Args:
+            value (T): The value object value.
+
+        Returns:
+            T: The processed value object value.
+        """
+        return value
+
+    def _validate(self, value: T) -> None:
+        """
+        This method validates that the value follows the domain rules.
+
+        Args:
+            value (T): The value object value.
+        """
+        pass
 
     @property
     def value(self) -> T:
