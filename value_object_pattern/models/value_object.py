d8f622dfcfdfc248fdcd5472c7a81fbb8a7bef5e
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-01-05 21:49:42 +0100

    🐛 fix: ValueObject validation and process were not following the designed order
    Additionally the performance was improved

--- value_object_pattern/models/value_object.py
+++ value_object_pattern/models/value_object.py
@@ -3,9 +3,9 @@ Value object generic type.
 """
 
 from abc import ABC
-from inspect import getmembers, ismethod
+from collections import deque
 from sys import version_info
-from typing import Generic, NoReturn, TypeVar
+from typing import Any, Callable, Generic, NoReturn, TypeVar
 
 if version_info >= (3, 12):
     from typing import override  # pragma: no cover
@@ -202,13 +202,9 @@ class ValueObject(ABC, Generic[T]):
         Returns:
             T: The processed value object value.
         """
-        methods = []
-        for _, method in getmembers(object=self, predicate=ismethod):
-            if getattr(method, '_is_process', False):
-                methods.append(method)
-
-        methods = sorted(methods, key=lambda method: getattr(method, '_order', method.__name__))
-        for method in methods:
+        methods = self._gather_decorated_methods(instance=self, attribute_name='_is_process')
+        while methods:
+            method: Callable[..., T] = methods.popleft().__get__(self, self.__class__)
             value = method(value=value)
 
         return value
@@ -221,14 +217,88 @@ class ValueObject(ABC, Generic[T]):
         Args:
             value (T): The value object value.
         """
-        methods = []
-        for _, method in getmembers(object=self, predicate=ismethod):
-            if getattr(method, '_is_validation', False):
-                methods.append(method)
-
-        methods = sorted(methods, key=lambda method: getattr(method, '_order', method.__name__))
-        for method in methods:
+        methods = self._gather_decorated_methods(instance=self, attribute_name='_is_validation')
+        while methods:
+            method: Callable[..., T] = methods.popleft().__get__(self, self.__class__)
             method(value=value)
+
+    def _gather_decorated_methods(self, instance: object, attribute_name: str) -> deque[Callable[..., Any]]:  # noqa: C901
+        """
+        Gathers decorated methods from instance.__class__ and its parent classes following the post-order DFS MRO,
+        returning them in a deque with the methods sorted by class hierarchy, method order, and method name.
+
+        Args:
+            instance (object): The object instance whose class hierarchy is inspected.
+            attribute_name (str): The attribute name used to identify the methods.
+
+        Returns
+            deque[Callable[..., Any]]: A deque of methods sorted by class hierarchy, method order, and method name.
+        """
+
+        def post_order_dfs_mro(cls: type, visited: set[type] | None = None, cut_off: type = object) -> list[type]:
+            """
+            Computes the Post-Order Depth-First Search (DFS) Method Resolution Order (MRO) of a class.
+
+            Args:
+                cls (type): The class to process.
+                visited (set[type] | None, optional): A set of already visited classes (to prevent duplicates). Defaults
+                to None.
+                cut_off (type, optional): The class to stop the search. Defaults to object.
+
+            Returns:
+                list[type]: A list of classes type sorted by post-order DFS MRO.
+            """
+            if cls is cut_off:
+                return []
+
+            if visited is None:
+                visited = set()
+
+            result = []
+            for parent in cls.__bases__:
+                if parent not in visited and parent is not object:  # pragma: no cover
+                    result.extend(post_order_dfs_mro(cls=parent, visited=visited, cut_off=cut_off))
+
+            if cls not in visited:  # pragma: no cover
+                visited.add(cls)
+                result.append(cls)
+
+            return result
+
+        classes = post_order_dfs_mro(cls=instance.__class__, cut_off=ValueObject)
+        classes_names = {cls.__name__: index for index, cls in enumerate(iterable=classes)}
+
+        classes_methods: deque[tuple[str, str, Callable[..., Any]]] = deque()
+        for cls in classes:
+            for method_name, method in cls.__dict__.items():
+                if not callable(method):
+                    continue
+
+                if not getattr(method, attribute_name, False):
+                    continue  # only methods with the attribute
+
+                classes_methods.append((method.__qualname__.split('.')[0], method_name, method))
+
+        def sort_key(item: tuple[str, str, Callable[..., Any]]) -> tuple[int, str, str]:
+            """
+            Sorts the methods by class hierarchy, method order attribute, and method name.
+            The only global variable used is classes_names.
+
+            Args:
+                item (tuple[str, str, Callable[..., Any]]): The item to sort.
+
+            Returns:
+                tuple[int, str, str]: A tuple with the class index, method order, and method name.
+            """
+            class_name, method_name, method = item
+            class_index = classes_names.get(class_name, 999)
+            order = getattr(method, '_order', method_name)
+
+            # print(class_index, order, method_name)
+            return int(class_index), order, method_name
+
+        # sort by class hierarchy, method order attribute, and method name
+        return deque([method for _, _, method in sorted(classes_methods, key=sort_key)])
 
     @property
     def value(self) -> T:
