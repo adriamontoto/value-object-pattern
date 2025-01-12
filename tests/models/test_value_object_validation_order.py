90cace272f18e31071457c45714dcfa2a8d9e742
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-01-12 16:56:34 +0100

    fix: conditionally import 'override' based on Python version

--- tests/models/test_value_object_validation_order.py
+++ tests/models/test_value_object_validation_order.py
@@ -2,7 +2,12 @@
 Test value object validation order.
 """
 
-from typing import override
+from sys import version_info
+
+if version_info >= (3, 12):
+    from typing import override  # pragma: no cover
+else:
+    from typing_extensions import override  # pragma: no cover
 
 from value_object_pattern import ValueObject, process
 
