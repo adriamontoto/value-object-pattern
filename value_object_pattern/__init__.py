cf2b52ef9b42efddb9d7c18c27886a1554d339ad
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-06-15 17:05:34 +0200

    feat: implement enumeration value object
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- value_object_pattern/__init__.py
+++ value_object_pattern/__init__.py
@@ -1,9 +1,10 @@
 __version__ = '0.2.0'
 
 from .decorators import process, validation
-from .models import ValueObject
+from .models import EnumerationValueObject, ValueObject
 
 __all__ = (
+    'EnumerationValueObject',
     'ValueObject',
     'process',
     'validation',
--- value_object_pattern/models/__init__.py
+++ value_object_pattern/models/__init__.py
@@ -1,3 +1,7 @@
+from .enumeration_value_object import EnumerationValueObject
 from .value_object import ValueObject
 
-__all__ = ('ValueObject',)
+__all__ = (
+    'EnumerationValueObject',
+    'ValueObject',
+)
