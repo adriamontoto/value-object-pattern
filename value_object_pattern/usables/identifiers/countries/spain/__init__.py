694573329c4429b77066776b437d22cffcda3ed7
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-08-05 10:58:02 +0200

    feat(identifiers): implement PhoneNumberValueObject
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- value_object_pattern/usables/identifiers/countries/spain/__init__.py
+++ value_object_pattern/usables/identifiers/countries/spain/__init__.py
@@ -2,10 +2,12 @@ from .dni_value_object import DniValueObject
 from .nie_value_object import NieValueObject
 from .nif_value_object import NifValueObject
 from .passport_value_object import PassportValueObject
+from .phone_number_value_object import PhoneNumberValueObject
 
 __all__ = (
     'DniValueObject',
     'NieValueObject',
     'NifValueObject',
     'PassportValueObject',
+    'PhoneNumberValueObject',
 )
