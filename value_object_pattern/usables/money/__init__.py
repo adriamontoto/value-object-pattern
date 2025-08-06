e3a5a02d30c12b4e52b628250fef7e3df6275556
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-08-06 13:19:34 +0200

    feat(money): implement CreditCardValueObject accepting Visa, Mastercard, Amex and Discovery cards
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- value_object_pattern/usables/money/__init__.py
+++ value_object_pattern/usables/money/__init__.py
@@ -1,3 +1,7 @@
+from .credit_card_value_object import CreditCardValueObject
 from .iban_value_object import IbanValueObject
 
-__all__ = ('IbanValueObject',)
+__all__ = (
+    'CreditCardValueObject',
+    'IbanValueObject',
+)
--- value_object_pattern/usables/money/iban_value_object.py
+++ value_object_pattern/usables/money/iban_value_object.py
@@ -34,7 +34,7 @@ class IbanValueObject(NotEmptyStringValueObject, TrimmedStringValueObject):
     @process(order=0)
     def _ensure_value_is_upper(self, value: str) -> str:
         """
-        Ensures the value object `value` is an upper string.
+        Ensures the value object `value` is stored in upper case.
 
         Args:
             value (str): The provided value.
