07cbaf831053773b62bf0da599c0b669dc14903d
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-08-06 09:48:55 +0200

    feat(identifiers): implement NussValueObject
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- value_object_pattern/usables/identifiers/countries/spain/__init__.py
+++ value_object_pattern/usables/identifiers/countries/spain/__init__.py
@@ -2,6 +2,7 @@ from .car_plate_value_object import CarPlateValueObject
 from .dni_value_object import DniValueObject
 from .nie_value_object import NieValueObject
 from .nif_value_object import NifValueObject
+from .nuss_value_object import NussValueObject
 from .passport_value_object import PassportValueObject
 from .phone_number_value_object import PhoneNumberValueObject
 
@@ -10,6 +11,7 @@ __all__ = (
     'DniValueObject',
     'NieValueObject',
     'NifValueObject',
+    'NussValueObject',
     'PassportValueObject',
     'PhoneNumberValueObject',
 )
