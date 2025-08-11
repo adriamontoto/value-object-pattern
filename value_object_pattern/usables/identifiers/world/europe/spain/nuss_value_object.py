721785d2569f7bef9cbe1637eb5d1bc1333e909b
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-08-11 13:59:39 +0200

    feat(nuss): implement province code validation
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- value_object_pattern/usables/identifiers/world/europe/spain/nuss_value_object.py
+++ value_object_pattern/usables/identifiers/world/europe/spain/nuss_value_object.py
@@ -8,6 +8,8 @@ from typing import NoReturn
 from value_object_pattern.decorators import process, validation
 from value_object_pattern.usables import NotEmptyStringValueObject, TrimmedStringValueObject
 
+from .utils import get_provincial_codes
+
 
 class NussValueObject(NotEmptyStringValueObject, TrimmedStringValueObject):
     """
@@ -72,6 +74,24 @@ class NussValueObject(NotEmptyStringValueObject, TrimmedStringValueObject):
             self._raise_value_is_not_social_security_number(value=value)
 
     @validation(order=2, early_process=True)
+    def _ensure_value_has_valid_province_code(self, value: str, processed_value: str) -> None:
+        """
+        Ensures the value object `value` has a valid province code.
+
+        Args:
+            value (str): The provided value.
+            processed_value (str): The early processed value.
+
+        Raises:
+            ValueError: If the `value` does not have a valid province code.
+        """
+        match = self._IDENTIFICATION_REGEX.fullmatch(string=processed_value)
+
+        province, _, _ = match.groups()  # type: ignore[union-attr]
+        if int(province) not in get_provincial_codes():
+            self._raise_value_is_not_social_security_number(value=value)
+
+    @validation(order=3, early_process=True)
     def _ensure_value_has_valid_control_letter(self, value: str, processed_value: str) -> None:
         """
         Ensures the value object `value` has a valid control letter.
